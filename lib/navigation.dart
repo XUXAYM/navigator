9d4bef7aa527f774ee6d3481f6f81a603f2a55dc
Andrei Belausau <andrushens@yandex-team.ru>
2024-02-05 02:57:23 +0300

    add navigator_key template

---
+++ lib/navigation.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'routes.dart';
+
+class NavigationManager {
+  NavigationManager._();
+
+  // TODO static final instance
+
+  // TODO final key
+
+  // TODO NavigatorState get _navigator
+
+  Future<int?> openCounter(String title, {required int lastCounter}) async {
+    // TODO
+  }
+
+  void pop() {
+    // TODO
+  }
+}
